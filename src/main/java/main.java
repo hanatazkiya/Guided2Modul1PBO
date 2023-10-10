03f16bb9cba2dfe03c5f9b25c4d05377cb3803ca
hanatazkiyaa <hanatazkiyaa31@gmail.com>
2023-10-10 15:55:33 +0700

    Initial

---
+++ src/main/java/main.java
@@ -0,0 +1,11 @@
+public class main {
+    //    public static void main(String []args)
+//    {
+//        Welcome W=new Welcome (); W.display();
+//    }
+    public static void main(String args[]) {
+        Mahasiswa M = new Mahasiswa();
+        M.inputData();
+        M.display();
+    }
+}
